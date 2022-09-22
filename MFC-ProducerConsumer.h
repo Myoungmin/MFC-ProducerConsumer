cb171275646973b6c0bce06a31c1e38228491fc5
Myoungmin <miung222@gmail.com>
2022-09-22 21:46:47 +0900

    Add project files.

---
+++ MFC-ProducerConsumer.h
@@ -0,0 +1,32 @@
+﻿
+// MFC-ProducerConsumer.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCProducerConsumerApp:
+// See MFC-ProducerConsumer.cpp for the implementation of this class
+//
+
+class CMFCProducerConsumerApp : public CWinApp
+{
+public:
+	CMFCProducerConsumerApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCProducerConsumerApp theApp;
